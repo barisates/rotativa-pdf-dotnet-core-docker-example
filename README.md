08558eb26b5676073a21c4dadba17776accb00ae
bates <bates@trabilisim.com>
2020-06-18 15:33:54 +0300

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # rotativa-pdf-dotnet-core-docker-example
 Rotativa PDF converter example running on Docker in .NET Core.
+
+This repository exactly based on this example https://github.com/webgio/Rotativa.AspNetCore
+
+As a difference, this example works on the **Docker Linux (bionic) container.**
---
+++ rotativa-dotnet-core/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace rotativa_dotnet_core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+
+            string htmlFile = Path.Combine(currentDirectory, "File", "test.html");
+
+            string pdfFile = Path.Combine(currentDirectory, "File", "test.pdf");
+
+            string workingDirectory = Path.Combine(currentDirectory, "Rotativa");
+
+            string html = File.ReadAllText(htmlFile);
+
+            byte[] pdfByteArr = WkhtmltopdfDriver.ConvertHtml(workingDirectory, "", html);
+
+            File.WriteAllBytes(pdfFile, pdfByteArr);
+
+            Console.WriteLine("Pdf Created");
+        }
+    }
+}
